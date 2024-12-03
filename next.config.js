398f2ca19f9ca0625fba1bed81bffb59e4fc5f04
Zizoubt <zbourghit@gmail.com>
2024-12-03 18:52:23 +0100

    Update next.config.js for image handling

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '77.37.122.81',
-        port: '3000',
-        pathname: '/uploads/**',
-      },
-    ],
+    domains: ['77.37.122.81'],
     unoptimized: true,
   },
   output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
 }
 
 module.exports = nextConfig 
