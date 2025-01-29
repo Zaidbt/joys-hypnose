94d3560bd51519ae908cdf104faa1153ac8ab884
Zaidbt <zaidbt@gmail.com>
2025-01-30 00:56:39 +0100

    fix: add connectToDatabase function

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -25,4 +25,10 @@ if (process.env.NODE_ENV === 'development') {
   clientPromise = client.connect();
 }
 
+export async function connectToDatabase() {
+  const client = await clientPromise;
+  const db = client.db('joyshypnose');
+  return { client, db };
+}
+
 export default clientPromise; 
